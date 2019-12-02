a8e622520b18cc1abf717befdea5aa68cfefa5cd
AlexisTarjuelo <Alexis.Tarjuelo@alu.uclm.es>
2019-12-02 19:07:21 +0100

    Creación escenario antes de implementación

---
+++ Features/ModificarHorarioTest.feature
@@ -0,0 +1,13 @@
+@featureTest
+Feature: Comprobar el correcto funcionamiento de la modificación de horarios
+
+	@Scenario1
+  Scenario: Al modificar el horario de un médico, se modifica dicho horario de la base de datos
+    Given Se registra el usuario
+    And Se registra un medico
+    When Se registra el horario del medico
+    Then El horario cambia en la base de datos
+    And Se elimina el usuario
+    And Se elimina el medico
+    And Se elimina el nuevo horario
+    
---
+++ src/test/java/com/tgs/tgh/stepDefinition/ModificacionHorarioTest.java
@@ -0,0 +1,15 @@
+package com.tgs.tgh.stepDefinition;
+
+import cucumber.api.java.en.Then;
+
+public class ModificacionHorarioTest {
+	@Then("^El horario cambia en la base de datos$")
+	public void el_horario_cambia_en_la_base_de_datos() throws Throwable {
+		// Write code here that turns the phrase above into concrete actions
+	}
+
+	@Then("^Se elimina el nuevo horario$")
+	public void se_elimina_el_nuevo_horario() throws Throwable {
+		// Write code here that turns the phrase above into concrete actions
+	}
+}
